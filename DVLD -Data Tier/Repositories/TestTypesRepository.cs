bc0a1938788d56d5f5c8ebb34e1735b499d07848
Yamen Muammar <yamenmuamar@gmail.com>
2026-02-25 23:13:58 +0200

    Create Test type files

---
+++ DVLD -Data Tier/Repositories/TestTypesRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD__Data_Tier.Repositories
+{
+    public class TestTypesRepository
+    {
+    }
+}
