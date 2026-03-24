59b72ef69a3b997d458f26bf3e1e4a81fc8d5b2b
Yamen Muammar <yamenmuamar@gmail.com>
2026-03-25 00:36:44 +0200

    FIX

--- DVLD -Presentation Tier/Controls/UserControls/ctrlUserInfo.cs
+++ DVLD -Presentation Tier/Controls/UserControls/ctrlUserInfo.cs
@@ -19,7 +19,7 @@ namespace DVLD__Presentation_Tier.Controls.UserControls
             InitializeComponent();
             _userService = new UserService();
             _personService = new PersonService();
-            _userId = -2;
+            _userId = -1;
         }
 
         public ctrlUserInfo(int UserId)
