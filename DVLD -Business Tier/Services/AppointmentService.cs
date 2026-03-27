fac74794ee6f8813c0d2fe822370b6a962ce2a4b
Yamen Muammar <yamenmuamar@gmail.com>
2026-03-27 14:35:08 +0200

    File Creation for Appointment Model

---
+++ DVLD -Business Tier/Services/AppointmentService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD__Business_Tier.Services
+{
+    public class AppointmentService
+    {
+        public AppointmentService()
+        {
+            
+        }
+    }
+}
